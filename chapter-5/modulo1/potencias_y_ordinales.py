f98f7bd48b7e41beb187cb45bae8f24338146b73
Federico Martinez <federico.martinez@coppel.com>
2024-04-18 12:13:58 -0600

    Se vieron formas de importar modulos o archivos. Y el uso de dounder's

---
+++ chapter-5/modulo1/potencias_y_ordinales.py
@@ -0,0 +1,48 @@
+import os
+import time
+
+def nth_root(radicand, n):
+    return radicand ** (1/n)
+
+def ordinal_suffix(value):
+    s = str(value)
+    if s.endswith('11'):
+        return 'th'
+    elif s.endswith('12'):
+        return 'th'
+    elif s.endswith('13'):
+        return 'th'
+    elif s.endswith('1'):
+        return 'st'
+    elif s.endswith('2'):
+        return 'nd'
+    elif s.endswith('3'):
+        return 'rd'
+    return 'th'
+
+def ordinal(value):
+    return str(value) + ordinal_suffix(value)
+
+def display_nht_root(radicand, n):
+    root = nth_root(radicand, n)
+    message = "The " + ordinal(n) + " root of " \
+              + str(radicand) + " is " + str(root)
+    print(message)
+
+
+
+# Si quiero probar mi archivo pero que no se ejecute lo que esta despues del if
+# cuando importamos este archivo como modulo en otro archivo se hace lo siguiente:
+if __name__ == "__main__":      # __name__ conteendra el nombre que se le asigna al modulo al momento de ejecutarse.
+    os.system('clear')          # Si recibe main, es que se se esta ejecutando de forma directa, y si recibe su propio
+    print('\n\n')               # nombre significa que esta siendo llamado mediante import en otro modulo o archivo.
+    time.sleep(5)
+    x = display_nht_root(27,4)  # Esto nos demuestra que una funcion que no devuelve un valor
+    print('x es igual a: ', x)  # siempre nos va a regresar None.
+    print('\n\n')
+    print('__name__ = ', __name__)
+    print('\n\n')
+else:
+    print('__name__ =', __name__) # Esto es solo pára probar que si se imprima el nombre del archivo desde otro modulo
+                                  # Si lo importo desde REPL se imprimira una sola o unica vez
+                                  # Si lo importo desde otro modulo se imprimira tantas veces como llame al modulo
---
+++ chapter-5/words.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+from time import sleep
+from os import system
+from modulo1 import potencias_y_ordinales as po
+
+
+def fetch_words():
+    story = urlopen('http://sixty-north.com/c/t.txt')
+    story_words = []
+    for line in story:
+        line_words = line.decode('utf8').split()    # Como la salida HTTP regresa bytes el resultado llega en binario por eso se decodifica a utf8
+        #line_words = line.split()                  # Si no se decodifica se imprime mas delante como su representacion en bytes
+        for word in line_words:
+            story_words.append(word)
+
+    story.close()
+    secs = 5
+    system('clear')
+    print("Wait until",secs,"seconds", end='', flush=True)
+    for second in range(secs+1):
+        sleep(1)
+        print(' .', end=' ', flush=True)
+        
+    print('\n')
+    for word in story_words:
+        print(word)  
+    print('\n')
+
+
+po.display_nht_root(25,2)
