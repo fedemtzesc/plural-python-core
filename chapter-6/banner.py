a3811aa72cba69f481c161350fbf42addecd412a
Federico Martinez <federico.martinez@coppel.com>
2024-04-25 11:52:09 -0600

    Se mexclaron los archivos banner

--- chapter-6/banner.py
+++ chapter-6/banner.py
@@ -1,30 +1,13 @@
-'''Retrieve and print words from a URL
-
-    Usage:
-        python3 banner.py header_text char
-'''
-
 import sys
 
-
 def banner(message, border='-'):
-    '''Returns an elegant header with borders uppon and buttom
-        
-        Args:
-            message: Is the message of the header
-            border: Is the character o string to use for the border
-
-        Returns:
-            A pretty and elegant header with borders
-    '''
     line = border * len(message)
-    line = border + line + border
-    print("\t",line)
-    print("\t ",message)
-    print("\t",line)
+    print(line)
+    print(message)
+    print(line)
 
 
 if __name__ == '__main__':
     msg = sys.argv[1]
     brd = sys.argv[2]
-    banner(msg, brd)
+
