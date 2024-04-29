7af3a9e099b88284b7ec92e90b71c3dfd32c8a04
Federico Martinez <federico.martinez@coppel.com>
2024-04-29 11:10:53 -0600

    Tipos de datos

--- chapter-6/banner.py
+++ chapter-6/banner.py
@@ -2,12 +2,20 @@ import sys
 
 def banner(message, border='-'):
     line = border * len(message)
-    print(line)
-    print(message)
-    print(line)
+    print("\t", line)
+    print("\t", message)
+    print("\t", line)
 
 
 if __name__ == '__main__':
-    msg = sys.argv[1]
-    brd = sys.argv[2]
+    if len(sys.argv)==3:
+        msg = sys.argv[1]
+        brd = sys.argv[2]
+    elif len(sys.argv)==2:
+        msg = sys.argv[1]
+        brd = "="
+    else:
+        msg = "NO INTRODUJO NINGUN ARGUMENTO POR LA LINEA DE COMANDOS"
+        brd = "="
 
+    banner(msg, brd)
